0b57fa36639241378f1f0da85a65dc434f5cacce
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-18 09:21:55 +0530

    Create holidays.js

---
+++ js/holidays.js
@@ -0,0 +1,38 @@
+
+
+// A more comprehensive list of holidays for 2024 and 2025.
+export const holidays = {
+    // --- 2024 Holidays ---
+    "2024-01-26": "Republic Day",
+    "2024-03-25": "Holi",
+    "2024-03-29": "Good Friday",
+    "2024-04-11": "Eid-ul-Fitr",
+    "2024-04-17": "Ram Navami",
+    "2024-05-23": "Buddha Purnima",
+    "2024-06-17": "Eid al-Adha",
+    "2024-07-17": "Muharram",
+    "2024-08-15": "Independence Day",
+    "2024-08-26": "Janmashtami",
+    "2024-10-02": "Gandhi Jayanti",
+    "2024-10-12": "Dussehra",
+    "2024-10-31": "Diwali (Deepavali)",
+    "2024-11-15": "Guru Nanak Jayanti",
+    "2024-12-25": "Christmas Day",
+
+    // --- 2025 Holidays ---
+    "2025-01-26": "Republic Day",
+    "2025-03-14": "Holi",
+    "2025-04-18": "Good Friday",
+    "2025-03-31": "Eid-ul-Fitr",
+    "2025-04-06": "Ram Navami",
+    "2025-05-12": "Buddha Purnima",
+    "2025-06-07": "Eid al-Adha",
+    "2025-07-07": "Muharram",
+    "2025-08-15": "Independence Day",
+    "2025-08-26": "Janmashtami",
+    "2025-10-02": "Gandhi Jayanti",
+    "2025-10-02": "Dussehra",
+    "2025-10-20": "Diwali (Deepavali)",
+    "2025-11-24": "Guru Nanak Jayanti",
+    "2025-12-25": "Christmas Day"
+};
